8551046f32c7ae50a59005bb0f4c97767e59b990
GLORE <cortesgloire@gmail.com>
2022-09-20 14:19:19 +0200

    Hot fix

--- frontend/resident/src/app/login/login.component.ts
+++ frontend/resident/src/app/login/login.component.ts
@@ -1,7 +1,10 @@
-import { Component } from "@angular/core"
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     standalone: true,
@@ -9,14 +12,26 @@ import {MatInputModule} from '@angular/material/input';
     imports: [
         MatFormFieldModule,
         MatSelectModule,
-        MatInputModule
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        CommonModule
     ],
     templateUrl: 'login.component.html',
     styleUrls: ['login.component.scss']
 })
 export class LoginComponent{
-    startMessage = "Bonjour";
 
+
+    startMessage = "Bonjour";
+    visible = false;
+
+    /**
+     * changeVisibility - chsnge the visibility of the password
+     */
+    changeVisibility() {
+        this.visible = !this.visible;
+    }
 
     /**
      * getStartMessage - return a greeting message
